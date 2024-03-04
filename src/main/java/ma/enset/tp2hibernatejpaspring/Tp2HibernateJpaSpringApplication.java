1cd6cc1588628a190546f03b0a96d878be7cdae4
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-03-04 21:25:52 +0100

    first

---
+++ src/main/java/ma/enset/tp2hibernatejpaspring/Tp2HibernateJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp2hibernatejpaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2HibernateJpaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp2HibernateJpaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/tp2hibernatejpaspring/Tp2HibernateJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.tp2hibernatejpaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2HibernateJpaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
