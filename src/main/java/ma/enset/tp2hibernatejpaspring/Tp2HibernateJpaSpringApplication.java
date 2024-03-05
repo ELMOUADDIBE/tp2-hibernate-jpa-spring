ddd3907e04ecedde11567a9fac78930015247cfd
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-03-05 21:34:31 +0100

    add CRUD ops

--- src/main/java/ma/enset/tp2hibernatejpaspring/Tp2HibernateJpaSpringApplication.java
+++ src/main/java/ma/enset/tp2hibernatejpaspring/Tp2HibernateJpaSpringApplication.java
@@ -20,14 +20,39 @@ public class Tp2HibernateJpaSpringApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+        //Ajouter des patients
         patientRepository.save(new Patient(-1, "Hassan", new Date(), false, 4));
         patientRepository.save(new Patient(-1, "Khalid", new Date(), false, 5));
         patientRepository.save(new Patient(-1, "Zaid", new Date(), true, 6));
+        patientRepository.save(new Patient(-1, "Younes", new Date(), false, 7));
+        patientRepository.save(new Patient(-1, "Zakaria", new Date(), true, 8));
+
+        //Afficher les patients
         List<Patient> patients = patientRepository.findAll();
         patients.forEach(p->{
             System.out.println(p.toString());
         });
+
+        //Consulter un patient, exemple id=2
         Patient patient = patientRepository.findById(Long.valueOf(2)).get();
-        System.out.println("**" + patient.getName() + "**" + patient.getDateNaissance());
+        System.out.println("*****");
+        System.out.println(patient.getId());
+        System.out.println(patient.getName());
+        System.out.println(patient.getDateNaissance());
+        System.out.println("*****");
+
+        //Rechercher des patients
+        List<Patient> patients2 = patientRepository.findByNameContains("Z");
+
+        //Modifier un patient
+        Patient patient2 = patientRepository.findById(Long.valueOf(3)).get();
+        patient2.setName("ZaidModifier");
+        patientRepository.save(patient2);
+        //Afficher
+        Patient patient3 = patientRepository.findById(Long.valueOf(3)).get();
+        System.out.println(patient3.getName());
+
+        //Supprimer un patient
+        patientRepository.deleteById(Long.valueOf(3));
     }
 }
