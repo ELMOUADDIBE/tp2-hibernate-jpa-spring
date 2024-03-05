c2653dc056ddcc6f825a6969a0160d1cc4b0325a
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-03-05 20:33:51 +0100

    patient class

---
+++ src/main/java/ma/enset/tp2hibernatejpaspring/entities/Patient.java
@@ -0,0 +1,21 @@
+package ma.enset.tp2hibernatejpaspring.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}
